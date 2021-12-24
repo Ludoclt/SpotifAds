38abccd7e8d03abf119b4e023503622d05312772
Ludoclt <70451460+Ludoclt@users.noreply.github.com>
2021-12-24 17:20:04 +0100

    Application source code

---
+++ AlertWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace SpotifAds
+{
+    /// <summary>
+    /// Logique d'interaction pour AlertWindow.xaml
+    /// </summary>
+    public partial class AlertWindow : Window
+    {
+        public AlertWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void WindowMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
